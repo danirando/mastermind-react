342150672f2d2e2bbc1f61a294a577865e50ebdf
danirando <danielerando@tiscali.it>
2025-06-01 08:35:52 +0200

    gioco completato

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,8 +4,44 @@ export default function HomePage() {
   const [value, setValue] = useState("");
   const [submittedValue, setSubmittedValue] = useState("");
   const [tentativi, setTentativi] = useState([]);
-
   const [randomNumber, setRandomNumber] = useState("");
+  const [esatte, setEsatte] = useState(0);
+
+  const verifica = (val) => {
+    let esatte = 0;
+    let presenti = 0;
+
+    const secretArr = randomNumber.split("");
+    const guessArr = val.split("");
+
+    // cifre giuste e al posto giusto
+    for (let i = 0; i < 4; i++) {
+      if (guessArr[i] === secretArr[i]) {
+        esatte++;
+        secretArr[i] = null;
+        guessArr[i] = null;
+      }
+    }
+
+    // cifre giuste ma nel posto sbagliato
+    for (let i = 0; i < 4; i++) {
+      if (guessArr[i] !== null) {
+        const index = secretArr.indexOf(guessArr[i]);
+        if (index !== -1) {
+          presenti++;
+          secretArr[index] = null;
+        }
+      }
+    }
+    console.log(esatte, presenti);
+    return { esatte, presenti };
+  };
+
+  const hasWon = (esatte) => {
+    if (esatte === 4) {
+      console.log("hai vinto");
+    }
+  };
 
   useEffect(() => {
     const randomNumber = String(Math.floor(1000 + Math.random() * 9000));
@@ -21,20 +57,14 @@ export default function HomePage() {
   const handleInputSubmit = (e) => {
     e.preventDefault();
     if (value.length === 4) {
-      setTentativi((prev) => [...prev, value]);
+      const result = verifica(value); // { esatte: X, presenti: Y }
+      setTentativi((prev) => [...prev, { valore: value, ...result }]);
       setSubmittedValue(value);
-      verifica(); // usa direttamente lo state
+      setEsatte(result.esatte);
       setValue("");
+      hasWon(result.esatte);
     } else {
       alert("Inserisci esattamente 4 cifre.");
-    }
-  };
-
-  const verifica = () => {
-    if (randomNumber.includes(value)) {
-      console.log("✅ giusto (è incluso)");
-    } else {
-      console.log("❌ sbagliato");
     }
   };
 
@@ -62,10 +92,34 @@ export default function HomePage() {
           <div
             key={i}
             className="container response-container d-flex justify-content-between">
-            <div>{tentativo}</div>
-            <div>qui ci vanno i simboli</div>
+            <div>{tentativo.valore}</div>
+            <div>
+              {Array.from({ length: tentativo.esatte }, (_, i) => (
+                <span key={"e" + i}>X</span>
+              ))}
+              {Array.from({ length: tentativo.presenti }, (_, i) => (
+                <span key={"p" + i}>0</span>
+              ))}
+            </div>
           </div>
         ))}
+      <div>
+        {esatte === 4 && (
+          <>
+            <div className="container won-container">
+              <div>Hai vinto! 🎉</div>
+            </div>
+            <div className="d-flex justify-content-center m-3">
+              {" "}
+              <button
+                className="btn btn-primary"
+                onClick={() => window.location.reload()}>
+                Nuova Partita
+              </button>
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 }
